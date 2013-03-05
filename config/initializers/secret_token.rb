216cc9c949f2f43bb7f353ff97a5fcac8c5aa387
Duc Trinh <duc.trinh@eastagile.com>
2013-03-06 01:40:04 +0700

    First init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Clockandclock::Application.config.secret_token = '4aebb2725069f86dbd37d65ee58b49fafd0cd5674d997eea6d25755230f9081f60c65c48ce2edfdb2bb8ec94fb24cd75f97197758b6892b1bc13e425895f48c8'
